6112de978b65c0169791601df90c2f390472ee5c
Steven Bass <stevenwbass@gmail.com>
2023-08-19 16:30:16 -0400

    stub out method to transform dag to mermaid flowchart / update unit test stub

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+export class Dictionary<TKey, TData> {
+}
+
+export class HashSet<TKey>{
+}
+
+export interface Dag<TKey, TData> {
+    data: Dictionary<TKey, TData>;
+    incomingEdges: Dictionary<TKey, HashSet<TKey>>;
+    outgoingEdges: Dictionary<TKey, HashSet<TKey>>;
+    count: number;
+}
+
+export const transformDagToFlowchart = function<TKey, TData>(dag: Dag<TKey, TData>): string {
+    // TODO: parse dag and generate flowchart string representation
+    return `flowchart TD`;
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,27 @@
-import { myPackage } from '../src';
+import { transformDagToFlowchart, Dictionary, HashSet } from '../src';
+
+class TestDag<TKey, TData> {
+  data: Dictionary<TKey, TData>;
+  incomingEdges: Dictionary<TKey, HashSet<TKey>>;
+  outgoingEdges: Dictionary<TKey, HashSet<TKey>>;
+  count: number;
+
+  constructor() {
+    this.data = new Dictionary<TKey, TData>();
+    this.incomingEdges = new Dictionary<TKey, HashSet<TKey>>();
+    this.outgoingEdges = new Dictionary<TKey, HashSet<TKey>>();
+    this.count = 0;
+  }
+}
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('transformDagToFlowchart', () => {
+    it('should return a string representing the mermaid flowchart', () => {
+      const dag = new TestDag<string, number>();
 
-      const result = myPackage(message);
+      const result = transformDagToFlowchart(dag);
 
-      expect(result).toMatch(message);
+      expect(result).toMatch("flowchart TD");
     });
   });
 });
